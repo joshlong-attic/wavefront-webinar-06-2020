131ce3cc353cffa6e196c315efc5480ac3c1a7ff
Josh Long <josh@joshlong.com>
2020-06-02 18:06:43 -0700

    take2

---
+++ service1/src/main/java/com/example/service1/Service1Application.java
@@ -0,0 +1,88 @@
+package com.example.service1;
+
+import brave.sampler.Sampler;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@SpringBootApplication
+public class Service1Application {
+
+    private final Counter counter;
+    private final ReservationRepository reservationRepository;
+
+    Service1Application(ReservationRepository rr, MeterRegistry registry) {
+        this.reservationRepository = rr;
+        this.counter = Counter
+                .builder("greetings-counter")
+                .tags("region", "test") // optional
+                .register(registry);
+    }
+
+    @Bean
+    WebClient webClient(
+            WebClient.Builder builder) {
+        return builder.build();
+
+    }
+
+    @Bean
+    Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> http(
+            ReservationRepository rr,
+            WebClient httpClient) {
+        return route()
+                .GET("/reservations", r -> {
+                    var data = rr.findAll() ;
+                    return ServerResponse.ok().body(data, Reservation.class);
+                })
+                .GET("/hello", r -> {
+                    this.counter.increment();
+
+                    Flux<String> stringFlux = httpClient
+                            .get()
+                            .uri("http://localhost:8081/hello-again/SpringFans")
+                            .retrieve()
+                            .bodyToFlux(String.class);
+
+                    return ServerResponse.ok().body(stringFlux, String.class);
+                })
+                .build();
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service1Application.class, args);
+    }
+
+}
+
+interface ReservationRepository extends ReactiveCrudRepository<Reservation, Integer> {
+
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Reservation {
+    @Id
+    private Integer id;
+    private String name;
+}
---
+++ service2/src/main/java/com/example/service2/Service2Application.java
@@ -0,0 +1,44 @@
+package com.example.service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+@EnableBinding(Source.class)
+public class Service2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service2Application.class, args);
+    }
+
+}
+
+@RestController
+class AnotherGreetingsRestController {
+
+    private final MessageChannel channel;
+
+    AnotherGreetingsRestController(Source source) {
+        this.channel = source.output();
+    }
+
+    @GetMapping("/hello-again/{name}")
+    Map<String, String> helloAgain(@PathVariable String name) {
+        var stringMessage = "Hello  " + name + "!";
+        var msg = MessageBuilder.withPayload(stringMessage).build();
+        this.channel.send(msg);
+        return Collections.singletonMap("greeting", stringMessage);
+    }
+
+
+}
